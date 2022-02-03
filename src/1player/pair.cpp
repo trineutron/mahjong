936d67f1847fd2bc86211e4da851b3d26e262b5d
trineutron <programgoo@gmail.com>
2022-02-04 01:25:31 +0900

    1雀頭が揃う確率

---
+++ src/1player/pair.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+constexpr int kind = 34, tile = 4 * kind, turn = 18, iter = 1000000;
+
+uint32_t xrand()
+{
+    static uint64_t x = 1;
+    x ^= x << 9;
+    x ^= x >> 7;
+    return (x * 0x123456789abcdef) >> 32;
+}
+
+void simulate(std::vector<int> &magic, std::vector<int> &real)
+{
+    std::vector<int> seen(kind), hand(kind);
+    std::vector<bool> draw(tile);
+    bool win_seen = false, win_real = false;
+    int k_first = xrand() % tile;
+    draw.at(k_first) = true;
+    seen.at(k_first / 4)++;
+    hand.at(k_first / 4)++;
+    for (int i = 0; i < turn; i++)
+    {
+        int k = xrand() % tile;
+        while (draw.at(k))
+        {
+            k = xrand() % tile;
+        }
+        draw.at(k) = true;
+        seen.at(k / 4)++;
+        hand.at(k / 4)++;
+        if (seen.at(k / 4) == 2)
+        {
+            win_seen = true;
+        }
+        if (hand.at(k / 4) == 2)
+        {
+            win_real = true;
+        }
+        hand.at(k / 4)--;
+        if (win_seen)
+        {
+            magic.at(i)++;
+        }
+        if (win_real)
+        {
+            real.at(i)++;
+        }
+    }
+}
+
+int main()
+{
+    std::vector<int> magic(turn), real(turn);
+    for (int i = 0; i < iter; i++)
+    {
+        simulate(magic, real);
+    }
+    std::cout << "turn" << '\t' << "magic" << '\t' << "real" << '\t' << "ratio" << std::endl;
+    for (int i = 0; i < turn; i++)
+    {
+        std::cout << i + 1 << '\t' << magic.at(i) << '\t' << real.at(i) << '\t' << double(magic.at(i)) / real.at(i) << std::endl;
+    }
+    return 0;
+}
