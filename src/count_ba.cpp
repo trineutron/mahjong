28006c54ab55e849d2b41a5996aa58cec9bca941
trineutron <programgoo@gmail.com>
2022-04-01 14:18:44 +0900

    高速化

--- src/count_ba.cpp
+++ src/count_ba.cpp
@@ -1,63 +1,50 @@
-#include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <iostream>
 
-const std::string text[]{
-    "1m", "2m", "3m", "4m", "r5m", "5m", "6m", "7m", "8m", "9m",
-    "1p", "2p", "3p", "4p", "r5p", "5p", "6p", "7p", "8p", "9p",
-    "1s", "2s", "3s", "4s", "r5s", "5s", "6s", "7s", "8s", "9s",
-    "東", "南", "西", "北", "白", "發", "中"};
+const std::string text[]{"1m",  "2m", "3m", "4m", "r5m", "5m", "6m",  "7m",
+                         "8m",  "9m", "1p", "2p", "3p",  "4p", "r5p", "5p",
+                         "6p",  "7p", "8p", "9p", "1s",  "2s", "3s",  "4s",
+                         "r5s", "5s", "6s", "7s", "8s",  "9s", "東",  "南",
+                         "西",  "北", "白", "發", "中"};
 
-int kind(int pai_num)
-{
+int kind(int pai_num) {
     int res = pai_num / 4;
-    for (int color = 0; color < 3; color++)
-    {
-        if (pai_num > 36 * color + 16)
-        {
+    for (int color = 0; color < 3; color++) {
+        if (pai_num > 36 * color + 16) {
             res++;
         }
     }
     return res;
 }
 
-int main()
-{
-    constexpr int len = 11;
+int main() {
+    constexpr int len = 12;
     std::ifstream ifs("../score/hounan4/merged.txt");
-    if (not ifs)
-    {
+    if (not ifs) {
         std::cerr << "ERROR" << std::endl;
         std::exit(1);
     }
     char prev[len] = "";
     int idx = 0, count_kyoku[16] = {}, count_ba[16] = {}, count_reach[16] = {};
-    while (ifs)
-    {
-        ifs >> prev[idx];
+    while (ifs) {
+        ifs.get(prev[idx]);
         idx++;
-        if (idx == len)
-        {
+        if (idx == len) {
             idx = 0;
-        }
-        char current[len];
-        for (int i = 0; i < len; i++)
-        {
-            current[i] = prev[(idx + i) % len];
         }
 
         bool good = true;
-        for (int i = 0; i < len; i++)
-        {
-            if (current[i] != "<INITseed=\""[i])
-            {
+        int idx_next = idx;
+        for (int i = 0; i < len; i++) {
+            if (prev[idx_next] != "<INIT seed=\""[i]) {
                 good = false;
                 break;
             }
+            idx_next++;
+            if (idx_next >= len) idx_next -= len;
         }
-
-        if (not good)
-        {
+        if (not good) {
             continue;
         }
 
@@ -74,21 +61,23 @@ int main()
         count_reach[reach]++;
     }
 
-    std::cout << "num" << '\t' << "kyoku" << '\t' << "ba" << '\t' << "reach" << std::endl;
-    for (int i = 0; i < 16; i++)
-    {
-        std::cout << i << '\t' << count_kyoku[i] << '\t' << count_ba[i] << '\t' << count_reach[i] << std::endl;
+    std::cout << "num" << '\t' << "kyoku" << '\t' << "ba" << '\t' << "reach"
+              << std::endl;
+    for (int i = 0; i < 16; i++) {
+        std::cout << i << '\t' << count_kyoku[i] << '\t' << count_ba[i] << '\t'
+                  << count_reach[i] << std::endl;
     }
 
     int total_kyoku = 0, sum_ba = 0, sum_reach = 0;
-    for (int i = 0; i < 16; i++)
-    {
+    for (int i = 0; i < 16; i++) {
         total_kyoku += count_kyoku[i];
         sum_ba += i * count_ba[i];
         sum_reach += i * count_reach[i];
     }
     std::cout << std::fixed << std::setprecision(5);
-    std::cout << "avg" << '\t' << total_kyoku << '\t' << (double)sum_ba / total_kyoku << '\t' << (double)sum_reach / total_kyoku << std::endl;
+    std::cout << "avg" << '\t' << total_kyoku << '\t'
+              << (double)sum_ba / total_kyoku << '\t'
+              << (double)sum_reach / total_kyoku << std::endl;
 
     return 0;
 }
--- src/count_discard.cpp
+++ src/count_discard.cpp
@@ -1,85 +1,69 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-const std::string text[]{
-    "1m", "2m", "3m", "4m", "r5m", "5m", "6m", "7m", "8m", "9m",
-    "1p", "2p", "3p", "4p", "r5p", "5p", "6p", "7p", "8p", "9p",
-    "1s", "2s", "3s", "4s", "r5s", "5s", "6s", "7s", "8s", "9s",
-    "東", "南", "西", "北", "白", "發", "中"};
+const std::string text[]{"1m",  "2m", "3m", "4m", "r5m", "5m", "6m",  "7m",
+                         "8m",  "9m", "1p", "2p", "3p",  "4p", "r5p", "5p",
+                         "6p",  "7p", "8p", "9p", "1s",  "2s", "3s",  "4s",
+                         "r5s", "5s", "6s", "7s", "8s",  "9s", "東",  "南",
+                         "西",  "北", "白", "發", "中"};
 
-int kind(int pai_num)
-{
+int kind(int pai_num) {
     int res = pai_num / 4;
-    for (int color = 0; color < 3; color++)
-    {
-        if (pai_num > 36 * color + 16)
-        {
+    for (int color = 0; color < 3; color++) {
+        if (pai_num > 36 * color + 16) {
             res++;
         }
     }
     return res;
 }
 
-int main()
-{
+int main() {
     constexpr int len = 2;
     std::ifstream ifs("../score/hounan4/merged.txt");
-    if (not ifs)
-    {
+    if (not ifs) {
         std::cerr << "ERROR" << std::endl;
         std::exit(1);
     }
     char prev[len] = "";
     int idx = 0, draw[37] = {}, discard[37] = {};
-    while (ifs)
-    {
-        ifs >> prev[idx];
+    while (ifs) {
+        ifs.get(prev[idx]);
         idx++;
-        if (idx == len)
-        {
+        if (idx == len) {
             idx = 0;
         }
         char current[len];
-        for (int i = 0; i < len; i++)
-        {
-            current[i] = prev[(idx + i) % len];
+        for (int i = 0; i < len; i++) {
+            current[i] = prev[(idx + i) & 1];
         }
 
-        if (current[0] != '<')
-        {
+        if (current[0] != '<') {
             continue;
         }
         bool good = false, is_draw = false, is_discard = false;
-        for (int i = 0; i < 4; i++)
-        {
-            if (current[1] == i + 'D')
-            {
+        for (int i = 0; i < 4; i++) {
+            if (current[1] == i + 'D') {
                 good = true;
                 is_discard = true;
                 break;
             }
-            if (current[1] == i + 'T')
-            {
+            if (current[1] == i + 'T') {
                 good = true;
                 is_draw = true;
                 break;
             }
         }
-        if (not good)
-        {
+        if (not good) {
             continue;
         }
 
         bool fst = true;
         int pai_num = 0;
         char digit;
-        while (true)
-        {
+        while (true) {
             ifs >> digit;
-            if (not isdigit(digit))
-            {
-                if (fst)
-                {
+            if (not isdigit(digit)) {
+                if (fst) {
                     pai_num = -1;
                 }
                 break;
@@ -88,24 +72,20 @@ int main()
             pai_num += digit - '0';
             fst = false;
         }
-        if (pai_num == -1)
-        {
+        if (pai_num == -1) {
             continue;
         }
-        if (is_draw)
-        {
+        if (is_draw) {
             draw[kind(pai_num)]++;
-        }
-        else if (is_discard)
-        {
+        } else if (is_discard) {
             discard[kind(pai_num)]++;
         }
     }
 
     std::cout << "text" << '\t' << "draw" << '\t' << "discard" << std::endl;
-    for (int i = 0; i < 37; i++)
-    {
-        std::cout << text[i] << '\t' << draw[i] << '\t' << discard[i] << std::endl;
+    for (int i = 0; i < 37; i++) {
+        std::cout << text[i] << '\t' << draw[i] << '\t' << discard[i]
+                  << std::endl;
     }
 
     return 0;
