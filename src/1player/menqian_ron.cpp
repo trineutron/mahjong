26f6765a677fa8d880d555783ddb13556c3be491
trineutron <programgoo@gmail.com>
2022-04-08 03:37:03 +0900

    門前手が上がれる確率

---
+++ src/1player/menqian_ron.cpp
@@ -0,0 +1,160 @@
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include "xrand.h"
+
+constexpr int kind = 34, tile = 4 * kind, player = 4, turn = 18, iter = 10000;
+
+const std::vector<int> value{1, 2, 3, 4, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 4, 3, 2,
+                             1, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0};
+
+xrand rng;
+std::uniform_int_distribution<> dist(0, tile - 1);
+
+int count_chows(std::vector<int> hand) {
+    int count = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 7; j++) {
+            const int idx = 9 * i + j;
+            const int k = std::min({hand[idx], hand[idx + 1], hand[idx + 2]});
+            count += k;
+            hand[idx] -= k;
+            hand[idx + 1] -= k;
+            hand[idx + 2] -= k;
+        }
+    }
+    return count;
+}
+
+int count_mianzi(std::vector<int> hand, int idx = 0) {
+    int count = count_chows(hand);
+    for (int h = 0; h < kind; h++) {
+        if (hand[h] < 3 or h < idx) continue;
+        hand[h] -= 3;
+        count = std::max(count, 1 + count_mianzi(hand, h + 1));
+        hand[h] += 3;
+    }
+    return count;
+}
+
+int count_mianzi_head(std::vector<int> hand) {
+    int count = count_mianzi(hand), count_head = -1;
+    for (int h = 0; h < kind; h++) {
+        if (hand[h] < 2) continue;
+        hand[h] -= 2;
+        count_head = std::max(count_head, count_mianzi(hand));
+        hand[h] += 2;
+        if (count_head == count) break;
+    }
+    if (count > count_head) count--;
+    return count;
+}
+
+void simulate(std::vector<int> &magic, std::vector<int> &real) {
+    std::vector<int> seen(kind), hand(kind), deck(kind, 4);
+    std::vector<bool> draw(tile);
+    bool win_seen = false, win_real = false;
+    for (int i = 0; i < 13; i++) {
+        int k = dist(rng);
+        while (draw.at(k)) {
+            k = dist(rng);
+        }
+        draw.at(k) = true;
+        seen.at(k / 4)++;
+        hand.at(k / 4)++;
+    }
+    for (int i = 0; i < turn; i++) {
+        for (int p = 0; p < player; p++) {
+            int k = dist(rng);
+            while (draw.at(k)) {
+                k = dist(rng);
+            }
+            draw.at(k) = true;
+            deck.at(k / 4)--;
+            seen.at(k / 4)++;
+            if (not win_seen and count_mianzi_head(seen) >= 4) {
+                win_seen = true;
+            }
+            if (p != 3) {
+                seen.at(k / 4)--;
+            }
+            hand.at(k / 4)++;
+            if (not win_real and count_mianzi_head(hand) >= 4) {
+                win_real = true;
+            }
+            if (p != 3 or win_real) {
+                hand.at(k / 4)--;
+            } else {
+                int chows_before = count_mianzi_head(hand);
+                auto v = value;
+                for (int j = 0; j < kind; j++) {
+                    if (hand.at(j) == 0) {
+                        continue;
+                    }
+                    int wait = 0;
+                    hand.at(j)--;
+                    for (int l = 0; l < kind; l++) {
+                        int count = deck.at(l);
+                        if (count == 0) {
+                            continue;
+                        }
+                        hand.at(l)++;
+                        for (int m = l; m < kind; m++) {
+                            int n = count;
+                            if (m != l) {
+                                n *= deck.at(m);
+                            } else {
+                                n *= deck.at(m) - 1;
+                                n /= 2;
+                            }
+                            if (n == 0) {
+                                continue;
+                            }
+                            hand.at(m)++;
+                            if (count_mianzi_head(hand) > chows_before) {
+                                wait += n;
+                            }
+                            hand.at(m)--;
+                        }
+                        hand.at(l)--;
+                    }
+                    hand.at(j)++;
+                    v.at(j) -= 100 * wait;
+                }
+
+                int x = -1;
+                for (int j = 0; j < kind; j++) {
+                    if (hand.at(j) == 0) {
+                        continue;
+                    }
+                    if (x < 0 or v.at(j) < v.at(x)) {
+                        x = j;
+                    }
+                }
+                hand.at(x)--;
+            }
+            if (p == 3 and win_seen) {
+                magic.at(i)++;
+            }
+            if (p == 3 and win_real) {
+                real.at(i)++;
+            }
+        }
+    }
+}
+
+int main() {
+    std::vector<int> magic(turn), real(turn);
+    for (int i = 0; i < iter; i++) {
+        simulate(magic, real);
+    }
+    std::cout << "turn" << '\t' << "magic" << '\t' << "real" << '\t' << "ratio"
+              << std::endl;
+    for (int i = 0; i < turn; i++) {
+        std::cout << i + 1 << '\t' << magic.at(i) << '\t' << real.at(i) << '\t'
+                  << double(magic.at(i)) / real.at(i) << std::endl;
+    }
+    return 0;
+}
