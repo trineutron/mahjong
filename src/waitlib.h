eb21d80c92bc481d17f00ae2f049ad469cba2f35
trineutron <programgoo@gmail.com>
2022-04-11 01:24:09 +0900

    引数の順番間違いを修正

--- src/waitlib.h
+++ src/waitlib.h
@@ -3,6 +3,6 @@
 using pattern_t = std::vector<std::vector<int>>;
 constexpr int PAIR7 = -1, YAO13 = -2;
 
-bool split_mianzi(std::vector<int> &, int, int, pattern_t &);
+bool split_mianzi(std::vector<int> &, pattern_t &, int, int);
 bool isagari(std::vector<int> &, pattern_t &, int);
 std::vector<int> list_wait(std::vector<int> &, pattern_t &, int);
