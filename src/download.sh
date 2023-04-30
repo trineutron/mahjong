b78209864302937d2166e5c31708cd7100e286d7
trineutron <programgoo@gmail.com>
2023-04-30 20:01:24 +0900

    ダウンロードコマンドを少し変更

--- src/download.sh
+++ src/download.sh
@@ -1,2 +1,1 @@
-zcat ../2020/scc2020*.html.gz | grep 四鳳南喰赤－ | egrep -o 'http.{52}' | sed -e s/"?log="/"log\/?"/g | xargs wget
-ls | xargs cat > merged.txt
+zcat ../2020/scc2020*.html.gz | grep 四鳳南喰赤－ | grep -E -o 'http.{52}' | sed -e s/"?log="/"log\/?"/g | wget -i - -O - > merged.txt
